425517a000b63d73b3d036b5bb73ad1f418425df
terrysams <terrysams@macbook-air>
2018-02-18 16:24:12 -0600

    create conversion class with static method

---
+++ GalToLit/src/galtolit/GalToLit.java
@@ -0,0 +1,26 @@
+/*
+ * Try This 1-1.
+ * This program converts gallons to liters
+ */
+package galtolit;
+
+/**
+ *
+ * @author terrysams
+ */
+class GalToLit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+        
+        gallons = 10;
+        liters = gallons *2.7854;
+        
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+    
+}
